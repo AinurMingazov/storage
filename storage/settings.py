896d7d090936c74f4c125d69cdcdef5439396317
Professional <anr.mgzv@gmail.com>
2022-01-27 19:06:35 +0300

    add: import_export

--- storage/settings.py
+++ storage/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -120,9 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- store/models.py
+++ store/models.py
@@ -35,10 +35,11 @@ class Category(models.Model):
         return reverse('store:tool_list_by_category',
                        args=[self.slug])
 
+
 class Tool(models.Model):
 
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(upload_to='tools/%Y/%m/%d',
                               blank=True)
     keeper = models.ForeignKey(Keeper, on_delete=models.SET_NULL,
@@ -47,12 +48,10 @@ class Tool(models.Model):
                                related_name='category', null=True)
     quantity = models.PositiveIntegerField(default=0)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
-
 
     def __str__(self):
         return self.name
@@ -62,7 +61,7 @@ class Tool(models.Model):
                        args=[self.id, self.slug])
 
     class Meta:
-        ordering = ('-updated',)
+        ordering = ('name',)
 
 
 class Operation(models.Model):
