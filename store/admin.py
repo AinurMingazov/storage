639a4d23a6cc7b127b95d5f20906c144820ef411
ainur <anr.mgzv@gmail.com>
2024-08-05 22:00:56 +0300

    add docker, update to python 3.12

--- store/admin.py
+++ store/admin.py
@@ -1,32 +1,6 @@
 from django.contrib import admin
 
 from store.models import *
-from import_export.admin import ImportExportActionModelAdmin
-from import_export import resources, fields
-from import_export.widgets import ForeignKeyWidget
-
-
-class ToolResource(resources.ModelResource):
-
-    class Meta:
-        model = Tool
-        fields = ('id', 'name', 'quantity', 'category', 'keeper',
-                    'available', 'price')
-
-
-class ToolAdmin(ImportExportActionModelAdmin):
-    resource_class = ToolResource
-    list_display = [field.name for field in Tool._meta.fields if field.name != "id"]
-    # inlines = [ToolImageInline]
-admin.site.register(Tool, ToolAdmin)
-
-# @admin.register(Tool)
-# class ToolAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'quantity', 'category', 'keeper',
-#                     'available', 'created', 'updated', 'price', 'slug']
-#     list_filter = ['available', 'created', 'updated']
-#     list_editable = ['available']
-#     prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Keeper)
